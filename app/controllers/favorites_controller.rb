e26ac5dc2a08e6362be1683dfa36b10aa22ad91d
Komnen Grujic <komnengrujic@gmail.com>
2020-11-26 13:28:40 +0100

    DOne with favorites but...

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,10 @@
 class FavoritesController < ApplicationController
+  def index
+    @favorites = Favorite.all
+  end
+
+  def create
+    @favorite = Favorite.create(user: current_user, product_id: params[:product_id])
+  end
+
 end
