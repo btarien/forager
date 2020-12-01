16a65c7d8e36c18f427b7ac3009642adad235c2c
NedijkoSipic <nesipic@hotmail.com>
2020-12-01 15:02:11 +0100

    ajax basket added

--- app/controllers/groceries_controller.rb
+++ app/controllers/groceries_controller.rb
@@ -4,7 +4,6 @@ class GroceriesController < ApplicationController
     @all_stores = Store.all.map {|store| store.name }
     @groceries = Grocery.all
     @all_favorites = Favorite.where(user: current_user)
-    @gro_counter = Grocery.where(user: current_user).size
     @stores = []
     @groceries.each do |grocery|
       product = grocery.store_product.product
