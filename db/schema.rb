2ea635f91c16f3a9144673154d172e4045dc153b
Britt Tarien <britt.tarien@gmail.com>
2020-12-01 20:54:31 +0100

    add code and category to products model and scraping fully working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_153836) do
+ActiveRecord::Schema.define(version: 2020_12_01_192628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 2020_12_01_153836) do
     t.string "ingredients"
     t.string "nutriscore_img"
     t.string "product_img"
+    t.string "code"
+    t.string "category"
   end
 
   create_table "store_products", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,21 +16,21 @@ User.create!(email: "alice@example.com", password: "123456", image: 'https://sou
 User.create!(email: "bob@example.com", password: "123456", image: 'https://source.unsplash.com/8PMvB4VyVXA/100x100')
 
 puts 'creating stores...'
-Store.create!(name: 'Aldi', address: 'Schwanthalerstraße 14, 80336 München', url: 'https://www.aldi-sued.de/de/suchergebnis.html?search=', name_class: '.product-title')
-Store.create!(name: 'Kaufland', address: 'Schwanthalerstraße 31, 80336 München', url: 'https://www.kaufland.de/suche.assortmentSearch.html?searchsubmit=true&q=', name_class: '.m-offer-tile__title')
-# Store.create!(name: 'Edeka', address: 'Schwanthalerstraße 31, 80336 München', url: 'https://e-center-knauer.edeka-shops.de/en/search?SearchTerm=', name_class: '.m-offer-tile__title')
+Store.create!(name: 'Aldi', address: 'Schwanthalerstraße 14, 80336 München', url: 'https://www.aldi-sued.de/de/suchergebnis/produkte.html?search=', name_class: '.product-title')
+# Store.create!(name: 'Kaufland', address: 'Schwanthalerstraße 31, 80336 München', url: 'https://www.kaufland.de/suche.assortmentSearch.html?searchsubmit=true&q=', name_class: '.m-offer-tile__title')
+Store.create!(name: 'Edeka', address: 'Schwanthalerstraße 31, 80336 München', url: 'https://e-center-knauer.edeka-shops.de/en/search?SearchTerm=', name_class: '.product-title')
 # Store.create!(name: 'Metro', address: 'Schwanthalerstraße 31, 80336 München', url: 'https://produkte.metro.de/shop/search?q=')
 
 puts 'creating products...'
-products = ["Yogurt"]
+products = ['Joghurt', 'nudeln']
 
 products.each do |product|
+  puts "\n\n#{product.upcase}"
   Store.all.each do |store|
+    puts "\n#{store.name.upcase}"
     file = Nokogiri::HTML(URI.open(store.url + product))
     count = 0
     file.search(store.name_class).each do |name|
-      count += 1
-      break if count > 5
       product_name = name.content.strip
       product_name.gsub!(/\s\d.*/, '')
       product_obj = Openfoodfacts::Product.search(product_name).first
@@ -54,34 +54,51 @@ products.each do |product|
           protein: nutriments['proteins'],
           sodium: nutriments['sodium']
         }
-        Product.create!(
+        new_product = Product.new(
           name: product_name,
           brand: product_obj.brands,
+          category: product,
+          code: product_obj.code,
           nutriscore: product_obj.nutriscore_grade,
           nutrition: nutrition.to_json,
           ingredients: product_obj.ingredients_text,
           nutriscore_img: "nutriscore-#{product_obj.nutriscore_grade}.svg",
           product_img: product_obj.image_small_url
         )
+        if new_product.save
+          sp = StoreProduct.create!(product: new_product, store: store)
+          count += 1
+          puts "#{count}. #{sp.product.name}"
+          break if count == 15
+        else
+          sp = StoreProduct.new(product: Product.find_by(name: product_name), store: store)
+          if sp.save
+            puts "\n#{product_name.upcase}\n"
+            sp = StoreProduct.create!(product: Product.find_by(code: product_obj.code), store: store)
+            count += 1
+            puts "#{count}. #{sp.product.name}"
+            break if count == 15
+          end
+        end
       end
     end
   end
 end
 
-puts 'creating store-products...'
-StoreProduct.create!(price: 2.5, product: Product.first, store: Store.first)
-StoreProduct.create!(price: 3.0, product: Product.second, store: Store.first)
-StoreProduct.create!(price: 0.5, product: Product.third, store: Store.first)
-StoreProduct.create!(price: 2.5, product: Product.fourth, store: Store.second)
-StoreProduct.create!(price: 3.0, product: Product.fifth, store: Store.second)
-StoreProduct.create!(price: 0.5, product: Product.last, store: Store.second)
-StoreProduct.create!(price: 0.5, product: Product.last, store: Store.first)
+# puts 'creating store-products...'
+# StoreProduct.create!(price: 2.5, product: Product.first, store: Store.first)
+# StoreProduct.create!(price: 3.0, product: Product.second, store: Store.first)
+# StoreProduct.create!(price: 0.5, product: Product.third, store: Store.first)
+# StoreProduct.create!(price: 2.5, product: Product.fourth, store: Store.second)
+# StoreProduct.create!(price: 3.0, product: Product.fifth, store: Store.second)
+# StoreProduct.create!(price: 0.5, product: Product.last, store: Store.second)
+# StoreProduct.create!(price: 0.5, product: Product.last, store: Store.first)
 
-puts 'creating groceries...'
-Grocery.create!(quantity: 2, user: User.first, store_product: StoreProduct.first)
-Grocery.create!(quantity: 1, user: User.first, store_product: StoreProduct.second)
-Grocery.create!(quantity: 6, user: User.first, store_product: StoreProduct.third)
+# puts 'creating groceries...'
+# Grocery.create!(quantity: 2, user: User.first, store_product: StoreProduct.first)
+# Grocery.create!(quantity: 1, user: User.first, store_product: StoreProduct.second)
+# Grocery.create!(quantity: 6, user: User.first, store_product: StoreProduct.third)
 
-puts 'creating favorites...'
-Favorite.create!(product: Product.first, user: User.first)
-Favorite.create!(product: Product.second, user: User.first)
+# puts 'creating favorites...'
+# Favorite.create!(product: Product.first, user: User.first)
+# Favorite.create!(product: Product.second, user: User.first)
