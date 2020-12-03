f437e481db05bdd557065d720140807f6da79206
NedijkoSipic <nesipic@hotmail.com>
2020-12-03 14:30:23 +0100

    meta tags done, not sure if everything is set properly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:image])
     devise_parameter_sanitizer.permit(:account_update, keys: [:image])
   end
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
